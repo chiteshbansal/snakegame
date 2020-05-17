2f70dd98c7e2148f69d1a216a775b18c99570ae3
chiteshbansal <63744720+chiteshbansal@users.noreply.github.com>
2020-05-18 01:15:56 +0530

    Update snake.js

--- snake.js
+++ snake.js
@@ -9,7 +9,7 @@ const box = 32;
 const ground =new Image();
 ground.src = "images/ground.png";
 
-
+let snakeheadcolor='green';
 
 
 const foodImg = new Image();
@@ -81,7 +81,142 @@ maze[38]={x:11*box,y:(10+3)*box}
 maze[39]={x:10*box,y:(10+3)*box}
 maze[40]={x:16*box,y:(3+3)*box}
 
+// Change the maze 
+function changeMaze()
+{
+	let btn = document.getElementById('mazebutton');
+	if(btn.className==="rotate1")
+	{
+		maze[0]={x:2*box,y:(3+3)*box}
+		maze[1]={x:2*box,y:(2+3)*box}
+		maze[2]={x:2*box,y:(1+3)*box}
+		maze[3]={x:3*box,y:(1+3)*box}
+		maze[4]={x:4*box,y:(1+3)*box}
+		maze[5]={x:14*box,y:(1+3)*box}
+		maze[6]={x:15*box,y:(1+3)*box}
+		maze[7]={x:16*box,y:(1+3)*box}
+		maze[8]={x:16*box,y:(2+3)*box}
+		maze[9]={x:16*box,y:(3+3)*box}
+		maze[10]={x:14*box,y:(13+3)*box}
+		maze[11]={x:15*box,y:(13+3)*box}
+		maze[12]={x:16*box,y:(11+3)*box}
+		maze[13]={x:16*box,y:(12+3)*box}
+		maze[14]={x:16*box,y:(13+3)*box}
+		maze[15]={x:2*box,y:(13+3)*box}
+		maze[16]={x:3*box,y:(13+3)*box}
+		maze[17]={x:2*box,y:(11+3)*box}
+		maze[18]={x:2*box,y:(12+3)*box}
+		maze[19]={x:2*box,y:(13+3)*box}
+		maze[20]={x:6*box,y:(5+3)*box}
+		maze[21]={x:6*box,y:(6+3)*box}
+		maze[22]={x:6*box,y:(7+3)*box}
+		maze[23]={x:6*box,y:(8+3)*box}
+		maze[24]={x:6*box,y:(9+3)*box}
+		maze[25]={x:6*box,y:(10+3)*box}
+		maze[26]={x:7*box,y:(10+3)*box}
+		maze[27]={x:8*box,y:(10+3)*box}
+		maze[28]={x:7*box,y:(5+3)*box}
+		maze[29]={x:8*box,y:(5+3)*box}
+		maze[30]={x:10*box,y:(5+3)*box}
+		maze[31]={x:11*box,y:(5+3)*box}
+		maze[32]={x:12*box,y:(5+3)*box}
+		maze[33]={x:12*box,y:(6+3)*box}
+		maze[34]={x:12*box,y:(7+3)*box}
+		maze[35]={x:12*box,y:(8+3)*box}
+		maze[36]={x:12*box,y:(9+3)*box}
+		maze[37]={x:12*box,y:(10+3)*box}
+		maze[38]={x:11*box,y:(10+3)*box}
+		maze[39]={x:10*box,y:(10+3)*box}
+		maze[40]={x:4*box,y:(13+3)*box}
+		btn.className='rotate2';
+	}else if (btn.className==='rotate2')
+	{	
+		maze[0]={x:1*box,y:(0+3)*box}
+		maze[1]={x:2*box,y:(1+3)*box}
+		maze[2]={x:3*box,y:(2+3)*box}
+		maze[3]={x:3*box,y:(3+3)*box}
+		maze[4]={x:3*box,y:(4+3)*box}
+		maze[5]={x:4*box,y:(2+3)*box}
+		maze[6]={x:5*box,y:(2+3)*box}
+		maze[7]={x:6*box,y:(2+3)*box}
+		maze[8]={x:17*box,y:(0+3)*box}
+		maze[9]={x:16*box,y:(1+3)*box}
+		maze[10]={x:15*box,y:(2+3)*box}
+		maze[11]={x:14*box,y:(2+3)*box}
+		maze[12]={x:13*box,y:(2+3)*box}
+		maze[13]={x:15*box,y:(3+3)*box}
+		maze[14]={x:15*box,y:(4+3)*box}
+		maze[15]={x:12*box,y:(2+3)*box}
+		maze[16]={x:1*box,y:(14+3)*box}
+		maze[17]={x:2*box,y:(13+3)*box}
+		maze[18]={x:3*box,y:(12+3)*box}
+		maze[19]={x:3*box,y:(11+3)*box}
+		maze[20]={x:3*box,y:(10+3)*box}
+		maze[21]={x:4*box,y:(12+3)*box}
+		maze[22]={x:5*box,y:(12+3)*box}
+		maze[23]={x:6*box,y:(12+3)*box}
+		maze[24]={x:17*box,y:(14+3)*box}
+		maze[25]={x:16*box,y:(13+3)*box}
+		maze[26]={x:15*box,y:(12+3)*box}
+		maze[27]={x:15*box,y:(11+3)*box}
+		maze[28]={x:15*box,y:(10+3)*box}
+		maze[29]={x:14*box,y:(12+3)*box}
+		maze[30]={x:13*box,y:(12+3)*box}
+		maze[31]={x:12*box,y:(12+3)*box}
+		maze[32]={x:1*box,y:(0+3)*box}
+		maze[33]={x:1*box,y:(0+3)*box}
+		maze[34]={x:1*box,y:(0+3)*box}
+		maze[35]={x:1*box,y:(0+3)*box}
+		maze[36]={x:1*box,y:(0+3)*box}
+		maze[37]={x:1*box,y:(0+3)*box}
+		maze[38]={x:1*box,y:(0+3)*box}
+		maze[39]={x:1*box,y:(0+3)*box}
+		maze[40]={x:1*box,y:(0+3)*box}
+		btn.className="rotate3";
 
+	}else if(btn.className==='rotate3')
+	{
+		for(i=0 ;i<=40;i++)
+		{
+			maze[i].x=17*box;
+			maze[i].y=(1)*box;
+		}
+
+		btn.className='rotate1';
+	}
+}
+
+
+// change snake head color when clicked
+function changecolor()
+{
+	let btn = document.getElementById('snakeheadcolor');
+	if(btn.className==='color1')
+	{
+		snakeheadcolor='yellow';
+		btn.className='color2';
+	}else if(btn.className==='color2')
+	{
+		snakeheadcolor='cyan';
+		btn.className='color3';
+	}else if(btn.className==='color3')
+	{
+		snakeheadcolor='purple';
+		btn.className='color4';
+	}else if(btn.className==='color4')
+	{
+		snakeheadcolor='red';
+		btn.className='color5';
+	}else if(btn.className==='color5')
+	{
+		snakeheadcolor='violet';
+		btn.className='color6';
+	}else if(btn.className==='color6')
+	{
+		snakeheadcolor='indigo';
+		btn.className='color1';
+	}
+}
 
 function drawmaze(){
 	
@@ -116,26 +251,7 @@ let score = 0 ;
 
 // control the snake
 let d ; 
-$(document).on("pagecreate","#pageone",function(){
-  $("p").on("swipeleft",function(){
-    alert("You swiped left!");
-  });                       
-});
-$(document).on("pagecreate","#pageone",function(){
-  $("p").on("swipedown",function(){
-    alert("You swiped down");
-  });                       
-});
-$(document).on("pagecreate","#pageone",function(){
-  $("p").on("swipeup",function(){
-    alert("You swiped up!");
-  });                       
-});
-$(document).on("pagecreate","#pageone",function(){
-  $("p").on("swiperight",function(){
-    alert("You swiped right!");
-  });                       
-});
+
 document.addEventListener("keydown",direction);
 
 function direction(event){
@@ -204,7 +320,7 @@ function draw(){
 
 	for(i =0 ;i< snake.length ;i++)
 	{
-		ctx.fillStyle=i==0?"green":"white";
+		ctx.fillStyle=i==0?snakeheadcolor:"white";
 		ctx.fillRect(snake[i].x,snake[i].y,box,box);
 		ctx.strokeStyle = 'white';
 		ctx.strokeRect(snake[i].x,snake[i].y,box,box);
